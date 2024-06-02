13a95f3eb88b31e4392435433ba5884b3d800e18
Aryan Trivedi <aryantrivedi.lko@outlook.com>
2024-06-02 10:31:55 +0530

    committing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sayble/api/environment.dart';
@@ -9,7 +7,7 @@ import 'package:sayble/themes/theme.dart';
 import 'package:sayble/themes/util.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setSystemUIOverlayStyle(
@@ -19,16 +17,18 @@ void main() {
     ),
   );
 
-  SystemChrome.setPreferredOrientations(
+  await SystemChrome.setPreferredOrientations(
     [
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ],
-  ).then(
-    (_) {
-      runApp(const Sayble());
-    },
   );
+
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final String userToken = prefs.getString('apiToken') ?? '';
+  Environment.userToken = userToken;
+
+  runApp(const Sayble());
 }
 
 class Sayble extends StatelessWidget {
@@ -36,23 +36,17 @@ class Sayble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     TextTheme textTheme = createTextTheme(context, "Fredoka", "ABeeZee");
     MaterialTheme theme = MaterialTheme(textTheme);
-    getUserToken();
+
     return MaterialApp(
       title: 'Sayble',
       debugShowCheckedModeBanner: false,
       theme: theme.light(),
       darkTheme: theme.dark(),
       themeMode: ThemeMode.dark,
-      home: Environment.userToken==''
-          ? const WelcomeScreen()
-          : const ProfileScreen(),
+      home: Environment.userToken.isEmpty ? const WelcomeScreen() : const ProfileScreen(),
     );
-  }
-
-  getUserToken() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    Environment.userToken = prefs.getString('apiToken') ?? '';
   }
 }
--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,12 +1,196 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
 
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(CupertinoIcons.back),
+          title: Text(
+            "aryan_.__",
+            style: GoogleFonts.fredoka(
+              color: Colors.white,
+              fontSize: width * 0.048,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(CupertinoIcons.pen),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(
+                vertical: width * 0.02, horizontal: width * 0.064),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: width * 0.1,
+                      backgroundImage:
+                      const AssetImage("lib/assets/jpegs/profile.jpg"),
+                    ),
+                    SizedBox(width: width * 0.086),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Container(
+                                  alignment: Alignment.center,
+                                  width: width * 0.12,
+                                  child: Text(
+                                    "0",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: width * 0.064,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  "Followers",
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.8),
+                                    fontSize: width * 0.032,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: width * 0.086),
+                            Column(
+                              children: [
+                                Container(
+                                  width: width * 0.12,
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "0",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: width * 0.064,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  "Following",
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.8),
+                                    fontSize: width * 0.032,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: width * 0.086),
+                            Column(
+                              children: [
+                                Container(
+                                  alignment: Alignment.center,
+                                  width: width * 0.12,
+                                  child: Text(
+                                    "254",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: width * 0.064,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  "Posts",
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.8),
+                                    fontSize: width * 0.032,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: height * 0.024),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "John Doe",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: width * 0.056,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(width: width * 0.04),
+                    Text(
+                      "he/him",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: width * 0.032,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: width * 0.01),
+                SizedBox(
+                  width: width * 0.8,
+                  child: Text(
+                    "To anyone that ever told you you're no good... they're no better. — Hayley Williams",
+                    maxLines: 4,
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.6),
+                      fontSize: width * 0.036,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                SizedBox(height: height * 0.024),
+                const TabBar(
+                  tabs: [
+                    Tab(text: "Posts"),
+                    Tab(text: "Reels"),
+                    Tab(text: "Tagged"),
+                  ],
+
+                ),
+                SizedBox(
+                  height: height * 0.56,
+                  child: const TabBarView(
+                    children: [
+                      Center(child: Text("Posts Content")),
+                      Center(child: Text("Reels Content")),
+                      Center(child: Text("Tagged Content")),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
